cdc439e057c516d85827974d6e49b16f21a26d55
Anton Khlynovskiy <subzey@gmail.com>
2020-03-11 20:35:27 +0300

    Stuff

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin env deno
 import { readFileStr } from 'https://deno.land/std/fs/mod.ts';
+import { basename } from 'https://deno.land/std/path/mod.ts';
 import { parse } from 'https://deno.land/std/flags/mod.ts';
 import { compile } from './mod.ts';
 
@@ -9,12 +10,17 @@ const args = parse(Deno.args, {
 });
 
 if (args.help || args._.length !== 1) {
-	console.log('Help.');
+	console.log('BrainWAck - a Brainfuck to WebAssembly compiler.');
+	console.log('Usage:');
+	console.log('\tdeno index.ts FILE [OPTIONS]');
+	console.log('Options:');
+	console.log('\t--output    WAT output filename. If omitted, the result is written to stdout.');
+	console.log('\t--sourcemap Source map output filename. Ignored if omitted.');
 	Deno.exit(1);
 }
 
 async function main() {
-	const { result, sourceMap } = compile(await readFileStr(args._[0]));
+	const { result, sourceMap } = compile(await readFileStr(args._[0]), basename(args._[0]) || undefined);
 
 	if (args.sourcemap) {
 		Deno.writeFile(args.sourcemap, new TextEncoder().encode(sourceMap));
